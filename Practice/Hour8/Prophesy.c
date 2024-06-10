642ef88753fc4a04580b28fd02ef0ec21a81a31b
Bi-Ma-GOoOD <xphlayyi@gmail.com>
2024-06-10 14:15:04 +0700

    Clandestina

---
+++ Practice/Hour8/Prophesy.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("Divine Prophesy : He is coming...\n");
+    printf("Protagonist : Who..?\n");
+    printf("Divine Prophesy : His name is \"Viego\".\n");
+}
