69d0f41cd8e3945cc8c1455f1adee51bdde7cab4
Bi-Ma-GOoOD <xphlayyi@gmail.com>
2024-08-28 00:10:22 +0700

    unFinished-ray-week08-v001

---
+++ 3rdYears/Y3T1/RayTracing/Week08/code/RT_material.py
@@ -0,0 +1,283 @@
+# material class
+import RT_utility as rtu
+import RT_ray as rtr
+import math
+import RT_texture as rtt
+
+def reflect(vRay, vNormal):
+    # return the perfect reflection direction
+    return vRay - vNormal*rtu.Vec3.dot_product(vRay, vNormal)*2.0
+
+def refract(vRay, vNormal, fRefractRatio):
+    cos_theta = min(rtu.Vec3.dot_product(-vRay, vNormal), 1.0)
+    sin_theta = math.sqrt(1.0 - cos_theta*cos_theta)
+    cannot_refract = fRefractRatio*sin_theta > 1.0
+
+    if cannot_refract or schlick(cos_theta, fRefractRatio) > rtu.random_double():
+        return reflect(vRay, vNormal)
+    else:
+        perpendiular_dir = (vRay + vNormal*cos_theta)*fRefractRatio
+        parallel_dir = vNormal*(-math.sqrt(math.fabs(1.0 - perpendiular_dir.len_squared())))
+        return perpendiular_dir + parallel_dir
+
+def halfvector(vView, vLight):
+    vH = (vView + vLight)*0.5
+    return vH
+
+def schlick(fCosine, fIOR):
+    r0 = (1-fIOR) / (1+fIOR)
+    r0 = r0*r0
+    return r0 + (1-r0)*math.pow(1-fCosine, 5)
+
+class Material:
+    def __init__(self) -> None:
+        pass
+
+    def scattering(self, rRayIn, hHinfo):
+        pass
+
+    def is_light(self):
+        return False
+
+class Lambertian(Material):
+    def __init__(self, cAlbedo) -> None:
+        super().__init__()
+        self.color_albedo = rtu.Color(cAlbedo.r(), cAlbedo.g(), cAlbedo.b())
+
+    def scattering(self, rRayIn, hHinfo):
+        scattered_direction = hHinfo.getNormal() + rtu.Vec3.random_vec3_unit()
+        if scattered_direction.near_zero():
+            scattered_direction = hHinfo.getNormal()
+
+        scattered_ray = rtr.Ray(hHinfo.getP(), scattered_direction)
+        attenuation_color = self.BRDF(rRayIn, scattered_ray, hHinfo)
+        return rtu.Scatterinfo(scattered_ray, attenuation_color)
+
+    def BRDF(self, rView, rLight, hHinfo):
+        attenuation_color = rtu.Color(self.color_albedo.r(), self.color_albedo.g(), self.color_albedo.b())
+        return attenuation_color
+
+
+# a mirror class
+class Mirror(Material):
+    def __init__(self, cAlbedo) -> None:
+        super().__init__()
+        self.color_albedo = rtu.Color(cAlbedo.r(), cAlbedo.g(), cAlbedo.b())
+
+    def scattering(self, rRayIn, hHinfo):
+        # generate a reflected ray
+        reflected_ray = rtr.Ray(hHinfo.getP(), reflect(rRayIn.getDirection(), hHinfo.getNormal()))
+
+        # get attenuation_color
+        attenuation_color = self.BRDF(rRayIn, reflected_ray, hHinfo)
+
+        # return scattering info
+        return rtu.Scatterinfo(reflected_ray, attenuation_color)
+
+    def BRDF(self, rView, rLight, hHinfo):
+        attenuation_color = rtu.Color(self.color_albedo.r(), self.color_albedo.g(), self.color_albedo.b())
+        return attenuation_color
+
+
+# A dielectric transparent material 
+class Dielectric(Material):
+    def __init__(self, cAlbedo, fIor) -> None:
+        super().__init__()
+        self.color_albedo = rtu.Color(cAlbedo.r(), cAlbedo.g(), cAlbedo.b())
+        self.IOR = fIor
+
+    def scattering(self, rRayIn, hHinfo):
+        refract_ratio = self.IOR
+        if hHinfo.front_face:
+            refract_ratio = 1.0/self.IOR
+
+        # generate a refracted ray
+        uv = rtu.Vec3.unit_vector(rRayIn.getDirection())
+        refracted_dir = refract(uv, hHinfo.getNormal(), refract_ratio)
+        scattered_ray = rtr.Ray(hHinfo.getP(), refracted_dir)
+
+        attenuation_color = self.BRDF(rRayIn, scattered_ray, hHinfo)
+        # return scattering info
+        return rtu.Scatterinfo(scattered_ray, attenuation_color)
+
+    def BRDF(self, rView, rLight, hHinfo):
+        attenuation_color = rtu.Color(self.color_albedo.r(), self.color_albedo.g(), self.color_albedo.b())
+        return attenuation_color
+
+# a texture
+class TextureColor(Material):
+    def __init__(self, color_or_texture) -> None:
+        super().__init__()
+        if isinstance(color_or_texture, rtu.Color):
+            self.color_albedo = rtt.SolidColor(color_or_texture)
+        else:
+            self.color_albedo = color_or_texture
+
+    def scattering(self, rRayIn, hHinfo):
+        scattered_direction = hHinfo.getNormal() + rtu.Vec3.random_vec3_unit()
+        if scattered_direction.near_zero():
+            scattered_direction = hHinfo.getNormal()
+
+        scattered_ray = rtr.Ray(hHinfo.getP(), scattered_direction)
+        attenuation_color = self.BRDF(rRayIn, scattered_ray, hHinfo)
+
+        return rtu.Scatterinfo(scattered_ray, attenuation_color)
+
+    def BRDF(self, rView, rLight, hHinfo):
+        attenuation_color = self.color_albedo.tex_value(hHinfo.u, hHinfo.v, hHinfo.point)
+        return attenuation_color
+
+
+# A metal class with roughness parameter
+class Metal(Material):
+    def __init__(self, cAlbedo, fRoughness) -> None:
+        super().__init__()
+        self.color_albedo = rtu.Color(cAlbedo.r(), cAlbedo.g(), cAlbedo.b())
+        self.roughness = fRoughness
+        if self.roughness > 1.0:
+            self.roughness = 1.0
+
+    def scattering(self, rRayIn, hHinfo):
+        # compute scattered ray based on the roughtness parameter
+        reflected_direction = reflect(rtu.Vec3.unit_vector(rRayIn.getDirection()), hHinfo.getNormal()) + rtu.Vec3.random_vec3_unit()*self.roughness
+        reflected_ray = rtr.Ray(hHinfo.getP(), reflected_direction)
+        attenuation_color = self.BRDF(rRayIn, reflected_ray, hHinfo)
+
+        # check if the reflected direction is below the surface normal
+        if rtu.Vec3.dot_product(reflected_direction, hHinfo.getNormal()) <= 1e-8:
+            attenuation_color = rtu.Color(0,0,0)
+
+        return rtu.Scatterinfo(reflected_ray, attenuation_color)
+
+    def BRDF(self, rView, rLight, hHinfo):
+        attenuation_color = rtu.Color(self.color_albedo.r(), self.color_albedo.g(), self.color_albedo.b())
+        return attenuation_color    
+
+"""1. Complete 'scattering()' and 'BRDF()' methods in the 'class Phong(Material)' to represent the Phong BRDF."""
+# Phong reflection model
+# fr = kd + ks*(R.V)^roughness
+class Phong(Material):
+    def __init__(self, cAlbedo, kd, ks, fAlpha) -> None:
+        super().__init__()
+        self.color_albedo = rtu.Color(cAlbedo.r(), cAlbedo.g(), cAlbedo.b())
+        self.kd = kd
+        self.ks = ks
+        self.alpha = fAlpha
+
+    def scattering(self, rRayIn, hHinfo):
+        reflected_direction = -hHinfo.getNormal()
+        # check if the reflected direction is below the surface normal
+        while rtu.Vec3.dot_product(reflected_direction, hHinfo.getNormal()) <= 1e-8:
+
+            # compute scattered ray
+            reflected_direction = hHinfo.getNormal() + rtu.Vec3.random_vec3_unit()
+            if reflected_direction.near_zero():
+                reflected_direction = hHinfo.getNormal()
+
+        reflected_ray = rtr.Ray(hHinfo.getP(), reflected_direction)
+        phong_color = self.BRDF(rRayIn, reflected_ray, hHinfo)
+
+        return rtu.Scatterinfo(reflected_ray, phong_color)
+
+    def BRDF(self, rView, rLight, hHinfo):
+        # calculate diffuse color
+        diff_color = rtu.Color(self.color_albedo.r(), self.color_albedo.g(), self.color_albedo.b()) * self.kd * (1.0/math.pi)
+        reflection_vector = rtu.Vec3.unit_vector(reflect(-rLight.getDirection(), hHinfo.getNormal()))
+        view_direction_vector = rtu.Vec3.unit_vector(-rView.getDirection())
+        r_dot_v = max(0.0, rtu.Vec3.dot_product(reflection_vector, view_direction_vector))
+        spec_color = rtu.Color(self.color_albedo.r(), self.color_albedo.g(), self.color_albedo.b()) * self.ks * math.pow(r_dot_v, self.alpha)
+
+        return diff_color + spec_color
+
+"""2. Implement 'class Blinn(Material)'."""
+# Blinn-Phong reflection model
+# fr = kd + ks*(H.N)^roughness
+class Blinn(Material):
+    def __init__(self, cAlbedo, kd, ks, fAlpha) -> None:
+        super().__init__()
+        self.color_albedo = rtu.Color(cAlbedo.r(), cAlbedo.g(), cAlbedo.b())
+        self.kd = kd
+        self.ks = ks
+        self.alpha = fAlpha
+
+    def scattering(self, rRayIn, hHinfo):
+        reflected_direction = -hHinfo.getNormal()
+        while rtu.Vec3.dot_product(reflected_direction, hHinfo.getNormal()) <= 1e-8:
+            reflected_direction = hHinfo.getNormal() + rtu.Vec3.random_vec3_unit()
+            if reflected_direction.near_zero():
+                reflected_direction = hHinfo.getNormal()
+
+        reflected_ray = rtr.Ray(hHinfo.getP(), reflected_direction)
+        blinn_color = self.BRDF(rRayIn, reflected_ray, hHinfo)
+
+        return rtu.Scatterinfo(reflected_ray, blinn_color)
+
+    def BRDF(self, rView, rLight, hHinfo):
+        # calculate diffuse color
+        diff_color = rtu.Color(self.color_albedo.r(), self.color_albedo.g(), self.color_albedo.b()) * self.kd * (1.0/math.pi)
+        half_way_vector = rtu.Vec3.unit_vector(halfvector(-rView.getDirection(), rLight.getDirection()))
+        h_dot_n = max(0.0, rtu.Vec3.dot_product(half_way_vector, hHinfo.getNormal()))
+        spec_color = rtu.Color(self.color_albedo.r(), self.color_albedo.g(), self.color_albedo.b()) * self.ks * math.pow(h_dot_n, self.alpha)
+
+        return diff_color + spec_color
+    
+"""3. Implement 'class CookTorrance(Material)'. The parameters and equations used in the Cook-Torrance model is in the link."""
+# Cook-Torrance BRDF model
+# fr = kd/pi + ks*(DFG/4(w_o.N * w_i.N))
+class CookTorrance(Material):
+    def __init__(self, cAlbedo, kd, ks, froughness, fIor) -> None:
+        super().__init__()
+        self.color_albedo = rtu.Color(cAlbedo.r(), cAlbedo.g(), cAlbedo.b())
+        self.kd = kd
+        self.ks = ks
+        self.roughness = froughness
+        self.IOR = fIor
+
+    def normal_distribution(self, froughness, h_dot_n):
+        a = math.pow(froughness, 2)
+        a2 = a * a
+        h_dot_n2 = h_dot_n * h_dot_n
+
+        denom = (h_dot_n2 * (a2 - 1.0) + 1.0)
+        return a2 / (math.pi * denom * denom)
+    
+    def geometry_smith(self, v_dot_n, l_dot_n, froughness):
+        def geometry_schlick_ggx(fCosine, froughness):
+            k = math.pow(froughness + 1.0, 2) / 8.0
+            return fCosine / (fCosine * (1.0 - k) + k)
+
+        return geometry_schlick_ggx(v_dot_n, froughness) * geometry_schlick_ggx(l_dot_n, froughness)
+
+    def scattering(self, rRayIn, hHinfo):
+        reflected_direction = -hHinfo.getNormal()
+        while rtu.Vec3.dot_product(reflected_direction, hHinfo.getNormal()) <= 1e-8:
+            reflected_direction = hHinfo.getNormal() + rtu.Vec3.random_vec3_unit()
+            if reflected_direction.near_zero():
+                reflected_direction = hHinfo.getNormal()
+
+        reflected_ray = rtr.Ray(hHinfo.getP(), reflected_direction)
+        cook_torrence = self.BRDF(rRayIn, reflected_ray, hHinfo)
+
+        return rtu.Scatterinfo(reflected_ray, cook_torrence)
+
+    def BRDF(self, rView, rLight, hHinfo):
+        n_vector = hHinfo.getNormal()
+        half_way_vector = rtu.Vec3.unit_vector(halfvector(-rView.getDirection(), rLight.getDirection()))
+        view_direction_vector = rtu.Vec3.unit_vector(-rView.getDirection())
+        v_dot_n = max(0.0, rtu.Vec3.dot_product(view_direction_vector, n_vector))
+        n_dot_l = max(0.0, rtu.Vec3.dot_product(n_vector, rLight.getDirection()))
+        h_dot_n = max(0.0, rtu.Vec3.dot_product(half_way_vector, n_vector))
+        v_dot_h = max(0.0, rtu.Vec3.dot_product(view_direction_vector, half_way_vector))
+        # call fresnel schlick
+        F = schlick(v_dot_h, self.IOR)
+        # call normal distribution
+        D = self.normal_distribution(h_dot_n, self.roughness)
+        # call geometry
+        G = self.geometry_smith(v_dot_n, n_dot_l, self.roughness)
+        # calculate diffuse color
+        full_cook = (F * D * G) / max(4 * v_dot_n * n_dot_l, 1e-8)
+        diff_color = rtu.Color(self.color_albedo.r(), self.color_albedo.g(), self.color_albedo.b()) * self.kd * (1.0/math.pi)
+        spec_color = rtu.Color(self.color_albedo.r(), self.color_albedo.g(), self.color_albedo.b()) * self.ks * full_cook
+
+        return diff_color + spec_color
+    
---
+++ 3rdYears/Y3T1/RayTracing/Week08/code/main.py
@@ -0,0 +1,139 @@
+import RT_utility as rtu
+import RT_camera as rtc
+import RT_renderer as rtren
+import RT_material as rtm
+import RT_scene as rts
+import RT_object as rto
+import RT_integrator as rti
+import RT_light as rtl
+import RT_texture as rtt
+
+def renderPhong():
+    main_camera = rtc.Camera()
+    main_camera.aspect_ratio = 16.0/9.0
+    main_camera.img_width = 480
+    main_camera.center = rtu.Vec3(0,0,0)
+    main_camera.samples_per_pixel = 100
+    main_camera.max_depth = 5
+    main_camera.vertical_fov = 60
+    main_camera.look_from = rtu.Vec3(-2, 2, 1)
+    main_camera.look_at = rtu.Vec3(0, 0, -1)
+    main_camera.vec_up = rtu.Vec3(0, 1, 0)
+
+    defocus_angle =0.0
+    focus_distance = 10.0
+    main_camera.init_camera(defocus_angle, focus_distance)
+    # add objects to the scene
+
+    tex_checker_bw = rtt.CheckerTexture(0.32, rtu.Color(.2, .2, .2), rtu.Color(.9, .9, .9))
+    mat_tex_checker_bw = rtm.TextureColor(tex_checker_bw)
+    """1.2 Render a scene with 3 balls in the scene. Each ball exhibits a Phong material with roughness parameters = 0.8, 8 and 30."""
+    mat_phong1 = rtm.Phong(rtu.Color(0.77, 1.0, 0.58), 0.25, 1.0, 0.8)      # green
+    mat_phong2 = rtm.Phong(rtu.Color(0.63, 0.87, 1.0), 0.25, 1.0, 8)        # blue
+    mat_phong3 = rtm.Phong(rtu.Color(1.0, 0.4, 0.0), 0.25, 1.0, 30)        # orange
+
+    world = rts.Scene()
+    world.add_object(rto.Sphere(rtu.Vec3(   0,-100.5,-1),  100, mat_tex_checker_bw))
+    world.add_object(rto.Sphere(rtu.Vec3(-1.0,   0.0,-1),  0.5, mat_phong1))    # left
+    world.add_object(rto.Sphere(rtu.Vec3(   0,   0.0,-1),  0.5, mat_phong2))    # center
+    world.add_object(rto.Sphere(rtu.Vec3( 1.0,   0.0,-1),  0.5, mat_phong3))    # right
+
+    dlight = rtl.Diffuse_light(rtu.Color(0.9, 0.9, 0.9))
+    world.add_object(rto.Sphere(rtu.Vec3(   0, 1.0, 0.0), 0.05, dlight))
+    # world.add_object(rto.Quad(rtu.Vec3(-0.5,1.5,-0.5), rtu.Vec3(0,0,-7.5), rtu.Vec3(3,0,-0.5), dlight))
+
+    intg = rti.Integrator()
+
+    renderer = rtren.Renderer(main_camera, intg, world)
+    renderer.render()
+    renderer.write_img2png('week08(1)_brdf_Phong-v005.png')    
+
+def renderBlinn():
+    main_camera = rtc.Camera()
+    main_camera.aspect_ratio = 16.0/9.0
+    main_camera.img_width = 480
+    main_camera.center = rtu.Vec3(0,0,0)
+    main_camera.samples_per_pixel = 100
+    main_camera.max_depth = 5
+    main_camera.vertical_fov = 60
+    main_camera.look_from = rtu.Vec3(-2, 2, 1)
+    main_camera.look_at = rtu.Vec3(0, 0, -1)
+    main_camera.vec_up = rtu.Vec3(0, 1, 0)
+
+    defocus_angle =0.0
+    focus_distance = 10.0
+    main_camera.init_camera(defocus_angle, focus_distance)
+    # add objects to the scene
+
+    tex_checker_bw = rtt.CheckerTexture(0.32, rtu.Color(.2, .2, .2), rtu.Color(.9, .9, .9))
+
+    mat_tex_checker_bw = rtm.TextureColor(tex_checker_bw)
+    """2.2 Render a scene with 3 balls in the scene. Each ball exhibits a Phong material with roughness parameters = 0.8, 8 and 30."""
+    mat_blinn1 = rtm.Blinn(rtu.Color(0.77, 1.0, 0.58), 0.25, 1.0, 0.08)      # green
+    mat_blinn2 = rtm.Blinn(rtu.Color(0.63, 0.87, 1.0), 0.25, 1.0, 8)        # blue
+    mat_blinn3 = rtm.Blinn(rtu.Color(1.0, 0.4, 0.0), 0.25, 1.0, 300)        # orange
+
+
+    world = rts.Scene()
+    world.add_object(rto.Sphere(rtu.Vec3(   0,-100.5,-1),  100, mat_tex_checker_bw))
+    world.add_object(rto.Sphere(rtu.Vec3(-1.0,   0.0,-1),  0.5, mat_blinn1))    # left
+    world.add_object(rto.Sphere(rtu.Vec3(   0,   0.0,-1),  0.5, mat_blinn2))    # center
+    world.add_object(rto.Sphere(rtu.Vec3( 1.0,   0.0,-1),  0.5, mat_blinn3))    # right
+
+    dlight = rtl.Diffuse_light(rtu.Color(0.9, 0.9, 0.9))
+    world.add_object(rto.Sphere(rtu.Vec3(   0, 1.0, 0.0), 0.05, dlight))
+    # world.add_object(rto.Quad(rtu.Vec3(-0.5,1.5,-0.5), rtu.Vec3(0,0,-7.5), rtu.Vec3(3,0,-0.5), dlight))
+
+    intg = rti.Integrator()
+
+    renderer = rtren.Renderer(main_camera, intg, world)
+    renderer.render()
+    renderer.write_img2png('week08(2)_brdf_Blinn-v006.png')    
+
+
+def renderLetMeCook():
+    main_camera = rtc.Camera()
+    main_camera.aspect_ratio = 16.0/9.0
+    main_camera.img_width = 480
+    main_camera.center = rtu.Vec3(0,0,0)
+    main_camera.samples_per_pixel = 100
+    main_camera.max_depth = 5
+    main_camera.vertical_fov = 60
+    main_camera.look_from = rtu.Vec3(-2, 2, 1)
+    main_camera.look_at = rtu.Vec3(0, 0, -1)
+    main_camera.vec_up = rtu.Vec3(0, 1, 0)
+
+    defocus_angle =0.0
+    focus_distance = 10.0
+    main_camera.init_camera(defocus_angle, focus_distance)
+    # add objects to the scene
+
+    tex_checker_bw = rtt.CheckerTexture(0.32, rtu.Color(.2, .2, .2), rtu.Color(.9, .9, .9))
+
+    mat_tex_checker_bw = rtm.TextureColor(tex_checker_bw)
+    """3.2 Reuse the above scene for this problem."""
+    mat_blinn1 = rtm.CookTorrance(rtu.Color(0.77, 1.0, 0.58), 0.25, 1.0, 0.08, 0.0)      # green
+    mat_blinn2 = rtm.CookTorrance(rtu.Color(0.63, 0.87, 1.0), 0.25, 1.0, 8, 0.0)        # blue
+    mat_blinn3 = rtm.CookTorrance(rtu.Color(1.0, 0.4, 0.0), 0.25, 1.0, 300, 0.0)        # orange
+
+
+    world = rts.Scene()
+    world.add_object(rto.Sphere(rtu.Vec3(   0,-100.5,-1),  100, mat_tex_checker_bw))
+    world.add_object(rto.Sphere(rtu.Vec3(-1.0,   0.0,-1),  0.5, mat_blinn1))    # left
+    world.add_object(rto.Sphere(rtu.Vec3(   0,   0.0,-1),  0.5, mat_blinn2))    # center
+    world.add_object(rto.Sphere(rtu.Vec3( 1.0,   0.0,-1),  0.5, mat_blinn3))    # right
+
+    dlight = rtl.Diffuse_light(rtu.Color(0.9, 0.9, 0.9))
+    world.add_object(rto.Sphere(rtu.Vec3(   0, 1.0, 0.0), 0.05, dlight))
+    # world.add_object(rto.Quad(rtu.Vec3(-0.5,1.5,-0.5), rtu.Vec3(0,0,-7.5), rtu.Vec3(3,0,-0.5), dlight))
+
+    intg = rti.Integrator()
+
+    renderer = rtren.Renderer(main_camera, intg, world)
+    renderer.render()
+    renderer.write_img2png('week08(3)_let_me_cook-v008.png')    
+
+if __name__ == "__main__":
+    # renderPhong()
+    # renderBlinn()
+    renderLetMeCook()
