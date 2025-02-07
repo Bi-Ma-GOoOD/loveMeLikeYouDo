acc1f8cb9c2173e0f0c9b518f0eff3ded1ee41ca
Bi-Ma-GOoOD <xphlayyi@gmail.com>
2025-02-07 15:47:40 +0700

    satisfied-hw-CV-lab07

--- 3rdYears/Y3T2/ComputerVision/Lab07-LeafClassification/Lab07_doingV001.m
+++ 3rdYears/Y3T2/ComputerVision/Lab07-LeafClassification/Lab07_doingV001.m
@@ -13,25 +13,29 @@ for i = 1:numImages
     % Step 01: Shape Extraction
     gray_image = rgb2gray(img);
 
-    shadding_pattern = imgaussfilt(gray_image, 195);
+    shadding_pattern = imgaussfilt(gray_image, 225);
     h = double(shadding_pattern);
     g = double(gray_image);
     f = g ./ h;
 
-    level = graythresh(f);
-    disp(level);
-    BW = imbinarize(f, level);
+    levelOtsu = graythresh(f);
+    % levelAdap = adaptthresh(f);
+    % disp(level);
+    BW = imbinarize(f, 0.99);
     BW = ~BW;
 
     % Use imfill for patch hole for post-processing
     BW_fill = imfill(BW, "holes");
+    figure(1);
+    subplot(5, 5, i);
+    imshow(BW_fill);
 
     % Step 02: Feature Extraction
-    STATS = regionprops(BW_fill, 'Area', 'Circularity', 'Eccentricity');
+    STATS = regionprops(BW_fill, 'Eccentricity', 'Circularity');
 
-    feature_vector_train(i, 1) = STATS.Area;
-    feature_vector_train(i, 2) = STATS.Circularity;
-    feature_vector_train(i, 3) = STATS.Eccentricity;
+    feature_vector_train(i, 1) = STATS.Circularity;
+    feature_vector_train(i, 2) = STATS.Eccentricity;
+    % feature_vector_train(i, 3) = STATS.FilledArea;
 end
 
 % Test Part
@@ -49,25 +53,28 @@ for i = 1:numImages
     % Step 01: Shape Extraction
     gray_image = rgb2gray(img);
 
-    shadding_pattern = imgaussfilt(gray_image, 195);
+    shadding_pattern = imgaussfilt(gray_image, 600);
     h = double(shadding_pattern);
     g = double(gray_image);
     f = g ./ h;
 
     level = graythresh(f);
-    disp(level);
-    BW = imbinarize(f, level);
+    % disp(level);
+    BW = imbinarize(f, 0.99);
     BW = ~BW;
+    figure(2);
+    subplot(3, 5, i);
+    imshow(BW);
 
     % Use imfill for patch hole for post-processing
     BW_fill = imfill(BW, "holes");
 
     % Step 02: Feature Extraction
-    STATS = regionprops(BW_fill, 'Area', 'Circularity', 'Eccentricity');
+    STATS = regionprops(BW_fill, 'Eccentricity', 'Circularity');
 
-    feature_vector_test(i, 1) = STATS.Area;
-    feature_vector_test(i, 2) = STATS.Circularity;
-    feature_vector_test(i, 3) = STATS.Eccentricity;
+    feature_vector_test(i, 1) = STATS.Circularity;
+    feature_vector_test(i, 2) = STATS.Eccentricity;
+    % feature_vector_test(i, 3) = STATS.FilledArea;
 end
 
 % Step03: Classification
@@ -80,6 +87,7 @@ predictedLabels = predict(Mdl,feature_vector_test);
 table(testLabel(:),predictedLabels (:),'VariableNames',{'TrueLabels','PredictedLabels'})
 
 % Confusion Matrix
+figure(3)
 confusionchart(testLabel, predictedLabels);
 
 
